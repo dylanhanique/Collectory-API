e5160fec424efce09cb22eff70b315e4403198e7
Dylan Hanique <dylan.hanique@proton.me>
2025-12-22 15:56:43 +0100

    chore: initial commit

---
+++ src/main/java/fr/dylanhanique/collectoryapi/CollectoryApiApplication.java
@@ -0,0 +1,13 @@
+package fr.dylanhanique.collectoryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectoryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectoryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/dylanhanique/collectoryapi/CollectoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dylanhanique.collectoryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectoryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
