a5344cdf18518eab2026877745cb6add3344b7be
Dylan Hanique <dylan.hanique@proton.me>
2025-12-30 15:27:44 +0100

    feat: add createUser route

--- src/main/java/fr/dylanhanique/collectoryapi/controller/UserController.java
+++ src/main/java/fr/dylanhanique/collectoryapi/controller/UserController.java
@@ -1,12 +1,13 @@
 package fr.dylanhanique.collectoryapi.controller;
 
+import fr.dylanhanique.collectoryapi.dto.CreateUserRequest;
 import fr.dylanhanique.collectoryapi.dto.UserResponse;
 import fr.dylanhanique.collectoryapi.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -17,6 +18,12 @@ public class UserController {
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserResponse createUser(@Valid @RequestBody CreateUserRequest dto) {
+        return userService.create(dto);
     }
 
     @GetMapping("/{id}")
