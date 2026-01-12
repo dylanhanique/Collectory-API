faf1ea27c5934e3aa168304e0eddcb00469fffe5
Dylan Hanique <dylan.hanique@proton.me>
2026-01-12 21:43:16 +0100

    test: remove useless test method

--- src/test/java/fr/dylanhanique/collectoryapi/CollectoryApiApplicationTests.java
+++ src/test/java/fr/dylanhanique/collectoryapi/CollectoryApiApplicationTests.java
@@ -1,13 +1,7 @@
 package fr.dylanhanique.collectoryapi;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CollectoryApiApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
