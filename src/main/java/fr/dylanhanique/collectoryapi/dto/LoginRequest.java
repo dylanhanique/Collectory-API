c96af06bad3f8de44c8223e5419f67bba9e5169d
Dylan Hanique <dylan.hanique@proton.me>
2026-01-12 21:32:37 +0100

    refactor: add NotBlank decorator to password property

--- src/main/java/fr/dylanhanique/collectoryapi/dto/LoginRequest.java
+++ src/main/java/fr/dylanhanique/collectoryapi/dto/LoginRequest.java
@@ -6,5 +6,5 @@ import jakarta.validation.constraints.Size;
 
 public record LoginRequest (
         @NotBlank @Email String email,
-        @Size(min = 8) String password
+        @NotBlank @Size(min = 8) String password
 ) {}
